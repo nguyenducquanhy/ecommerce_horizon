95e08dda42238215a924b805e35b4aefa85f50bc
nguyen duc quan <quanhy2@gmail.com>
2023-05-27 01:17:53 +0700

    add testObject

--- testObject.php
+++ testObject.php
@@ -1,25 +1,40 @@
 <?php
+    include'library/connect.php';
     class user{
+        public $id;
+        public $name;
+        public $age;
         
-
+        public function __construct($id,$name,$age)
+        {
+            $this->id=$id;
+            $this->name=$name;
+            $this->age = $age;   
+        }
 
     }
 
+    $json = file_get_contents('php://input');
+    $data = json_decode($json,true);
+    $array=array();
 
-
-    //include'library/cors.php';
-    include'library/connect.php';
-
-    $value=$_POST["value"];
     
-    // $array=array();
+    for ($i=0; $i < sizeof($data); $i++) {
 
-    // foreach($item as $value){
-    //     echo json_encode($item,JSON_UNESCAPED_UNICODE);
-    //     array_push($array,$item);
-    // }
+        $item=$data[$i];            
+        
+        $id=$item['id'];        
+        $name = $item['name'];
+        $age = $item['age'];
 
-    echo json_encode($value,JSON_UNESCAPED_UNICODE)
+        $value=new user($id,$name,$age);
+
+        array_push($array,$value);    
+
+    }
+
+    echo json_encode($array,JSON_UNESCAPED_UNICODE);
+
 
 
 ?>
