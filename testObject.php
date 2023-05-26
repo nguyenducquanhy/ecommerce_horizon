c6276247607a27d67460bd9eb7f0c5c859e94c4d
nguyen duc quan <quanhy2@gmail.com>
2023-05-26 23:26:07 +0700

    update

--- testObject.php
+++ testObject.php
@@ -1,9 +1,9 @@
 <?php
-    include'library/cors.php';
+    //include'library/cors.php';
     include'library/connect.php';
 
     $value=$_POST["value"];
-    
+
     echo json_encode($value,JSON_UNESCAPED_UNICODE)
 
 
