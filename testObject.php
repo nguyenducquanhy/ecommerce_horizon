8465cad9a4be9b1d97240d1e12e372632568f17e
nguyen duc quan <quanhy2@gmail.com>
2023-05-27 00:03:34 +0700

    update

--- testObject.php
+++ testObject.php
@@ -12,14 +12,14 @@
 
     $value=$_POST["value"];
     
-    $array=array();
+    // $array=array();
 
-    foreach($item as $value){
-        echo json_encode($item,JSON_UNESCAPED_UNICODE);
-        array_push($array,$item);
-    }
+    // foreach($item as $value){
+    //     echo json_encode($item,JSON_UNESCAPED_UNICODE);
+    //     array_push($array,$item);
+    // }
 
-    echo json_encode($array,JSON_UNESCAPED_UNICODE)
+    echo json_encode($value,JSON_UNESCAPED_UNICODE)
 
 
 ?>
