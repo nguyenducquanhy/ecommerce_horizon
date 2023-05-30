b1332965034c8c149065caa1bacb41378ba775d7
nguyen duc quan <quanhy2@gmail.com>
2023-05-30 23:08:25 +0700

    add lasst

--- testResfullApi.php
+++ testResfullApi.php
@@ -1,18 +1,6 @@
 <?php
 
-    class User{
-        public $id;
-        public $name;
-        public $age;
-        
-        public function __construct($id,$name,$age)
-        {
-            $this->id=$id;
-            $this->name=$name;
-            $this->age = $age;   
-        }
-
-    }
+ 
 
     include'library/cors.php';
     if($_SERVER['REQUEST_METHOD']==='GET'){
@@ -39,29 +27,19 @@
     function postMethod(){
         $json = file_get_contents('php://input');
         $data = json_decode($json,true);
-        $item=$data['postValue'];
-        
-        $id=$item['id'];        
-        $name = $item['name'];
-        $age = $item['age'];
-
-        $value=new User($id,$name,$age);
-
-        echo json_encode($value ,JSON_UNESCAPED_UNICODE);
+        echo json_encode("post :".$data ,JSON_UNESCAPED_UNICODE);
     }
 
     function putMethod(){
         $json = file_get_contents('php://input');
         $data = json_decode($json,true);
-        $value=$data['putValue'];
-        echo json_encode("put".$value,JSON_UNESCAPED_UNICODE);
+        echo json_encode("put ".$data ,JSON_UNESCAPED_UNICODE);
     }
 
     function deleteMethod(){
         $json = file_get_contents('php://input');
         $data = json_decode($json,true);
-        $value=$data['deleteValue'];
-        echo json_encode("delete".$value,JSON_UNESCAPED_UNICODE);
+        echo json_encode("delete ".$data,JSON_UNESCAPED_UNICODE);
     }
 
 ?>
