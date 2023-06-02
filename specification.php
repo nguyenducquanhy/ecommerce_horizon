037f8f244467a481ac6c2a2e95fdf6f55ed92113
nguyen duc quan <quanhy2@gmail.com>
2023-06-02 14:38:59 +0700

    add pecification

---
+++ specification.php
@@ -0,0 +1,34 @@
+<?php
+include'library/cors.php';
+include'library/connect.php';
+
+if($_SERVER['REQUEST_METHOD']==='POST'){
+
+    $IdCPUInput =$_POST['IdCPUInput'];
+    $IdRAMInput =$_POST['IdRAMInput'];
+    $IdDISKInput =$_POST['IdDISKInput'];
+    $IdVGAInput=$_POST['IdVGAInput'];
+    $IdSCREENInput =$_POST['IdSCREENInput'];
+    $IdCOLORInput =$_POST['IdCOLORInput'];
+    $IdOSInput=$_POST['IdOSInput'];
+
+    $query="call login('$IdCPUInput','$IdRAMInput','$IdDISKInput','$IdVGAInput',
+                    '$IdSCREENInput','$IdCOLORInput','$IdOSInput')";
+    $result=mysqli_query($connect,$query);
+
+    if($result){
+                
+        $row=mysqli_fetch_row($result);
+        
+        echo json_encode ($row, JSON_UNESCAPED_UNICODE);
+          
+    }
+    else{
+        echo 504;
+    }
+
+
+}
+
+
+?>
