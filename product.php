d2e1f52ccd6e1cf1a17bca82902a1ad3b4601d3f
nguyen duc quan <quanhy2@gmail.com>
2023-06-02 14:37:16 +0700

    add specifi and product

---
+++ product.php
@@ -0,0 +1,25 @@
+<?php
+include'library/cors.php';
+include'library/connect.php';
+
+
+
+
+
+if($_SERVER['REQUEST_METHOD']==='GET'){
+    getMethod();
+}
+
+if($_SERVER['REQUEST_METHOD']==='POST'){
+    postMethod();
+}
+
+if($_SERVER['REQUEST_METHOD']==='PUT'){
+    putMethod();
+}
+
+if($_SERVER['REQUEST_METHOD']==='DELETE'){
+    deleteMethod();
+}
+
+?>
