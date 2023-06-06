fc2d4d9181673c01c24f40b49d16bbf635197493
nguyen duc quan <quanhy2@gmail.com>
2023-06-07 02:38:07 +0700

    add pagination user

--- getUser.php
+++ getUser.php
@@ -27,33 +27,53 @@ class infor{
     }
 }
     
+class pagination{
+    public $_page;
+    public $_limit;
+    public $_totalRows;
+
+    public function __construct ($_page, $_limit, $_totalRows){
+        $this->_page=$_page;
+        $this->_limit=$_limit;
+        $this->_totalRows=$_totalRows;
+    }
+
+}
+
     include'library/cors.php';
     include'library/connect.php';
-    
-     $query="select * from User where idRole <> '2'"; 
 
-    $result=mysqli_query($connect,$query);
-    $array=array();
+
+    $curentPage =$_GET["page"];   
+    $limitLoad =$_GET["limit"];   
+
+    $queryUserProfile="Call getAllUser('$curentPage','$limitLoad');";
+    $queryCountUserProfile="Call getCountForAllUser('$curentPage','$limitLoad')";
+
     
-    if($result){
-        
-        
-        while($row=mysqli_fetch_array($result)){
-            
-            
-            array_push($array,new infor($row['idRole'],$row['username'], $row['fullname'],$row['email'],
+    $resultUserProfile=mysqli_query($connect,$queryUserProfile)or die(mysqli_error($connect));
+    while(mysqli_next_result($connect)){;}
+
+    $resultPaginationUserProfile=mysqli_query($connect,$queryCountUserProfile)or die(mysqli_error($connect));
+    while(mysqli_next_result($connect)){;}
+
+    $arrayUser=array();
+    if($resultUserProfile && $resultPaginationUserProfile){      
+        while($row=$resultUserProfile->fetch_assoc()){       
+            array_push($arrayUser,new infor($row['idRole'],$row['username'], $row['fullname'],$row['email'],
             $row['idGender'],$row['DateOfBirth'],$row['PhoneNumber']));
-            
         }
 
-        
-     echo json_encode($array,JSON_UNESCAPED_UNICODE );
-
+        $row=mysqli_fetch_assoc($resultPaginationUserProfile);  
+        $newCountingUser=new pagination($row['_page'],$row['_limit'], $row['_totalRows']); 
+           
+        echo json_encode(
+            Array("data"=>$arrayUser,
+            "pagination"=>$newCountingUser),JSON_UNESCAPED_UNICODE );
     }
     else{
-        echo "empty!";
-    }
-
+        echo 504;
+    }    
 
 ?>
 
