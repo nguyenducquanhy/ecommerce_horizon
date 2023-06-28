a5225b5caa0b8f683dfab80ad54a35ad40a82bdc
nguyen duc quan <quanhy2@gmail.com>
2023-06-28 18:30:43 +0700

    update code1

--- testProductAndroid.php
+++ testProductAndroid.php
@@ -7,16 +7,14 @@ class product{
     public $Slug;
     public $CurrentPrice; 
     public $image;
-    public $Specifications;
 
-    public function __construct ($idProduct,$Category,$TradeMark,
-                                $Specifications,$Name,$Slug,
+
+    public function __construct ($idProduct,$Category,$TradeMark,$Name,$Slug,
                                 $CurrentPrice,$image){
                                     
         $this ->idProduct = $idProduct;
         $this ->Category = $Category;
         $this ->TradeMark = $TradeMark;
-        $this ->Specifications = $Specifications;
         $this ->NameProduct = $Name;
         $this ->Slug = $Slug;
         $this ->CurrentPrice = $CurrentPrice;
@@ -26,29 +24,7 @@ class product{
 
 
 }
-class Specifications{
 
-    public $CpuName;
-    public $RamName;
-    public $DiskName;
-    public $VgaName;
-    public $ScreenName;
-    public $ColorName;
-    public $OsName;
-    
-    public function __construct ($CpuName, $RamName, $DiskName, $VgaName, 
-                                $ScreenName, $ColorName, $OsName){
-
-        $this ->CpuName = $CpuName;
-        $this ->RamName = $RamName;
-        $this ->DiskName = $DiskName;
-        $this ->VgaName = $VgaName;
-        $this ->ScreenName = $ScreenName;
-        $this ->ColorName = $ColorName;
-        $this ->OsName = $OsName;     
-    }
-
-}
 
 include'library/cors.php';
 include'library/connect.php';
@@ -60,12 +36,11 @@ $result=mysqli_query($connect,$query)or die(mysqli_error($connect));
 if($result){      
     $array=array();
     while($row=$result->fetch_array()){
-        $Specifications=new Specifications($row['CpuName'],$row['RamName'],$row['DiskName'],$row['VgaName'],
-        $row['ScreenName'],$row['ColorName'],$row['OsName']);
+  
    
        $DetailProduct=new product(
            $row['Id'],$row['Category'], $row['TradeMark'],
-           $Specifications,$row['Name'],$row['Slug'],
+           $row['Name'],$row['Slug'],
            $row['CurrentPrice'],$row["image"]);            
            array_push($array,$DetailProduct);
     }
