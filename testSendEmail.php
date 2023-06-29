86241095daf634c1ec51a1ca40d2cd0247d5bde4
nguyen duc quan <quanhy2@gmail.com>
2023-06-29 22:35:05 +0700

    add log

--- testSendEmail.php
+++ testSendEmail.php
@@ -12,7 +12,7 @@ use PHPMailer\PHPMailer\Exception;
 require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
-
+echo 1;
 try {
     //Server settings
     $mail->SMTPDebug = 2;                               
@@ -23,20 +23,20 @@ try {
     $mail->Password = 'xzrqfpdwidjqwqbi';   // Your App Password
     $mail->SMTPSecure = 'tls';                         
     $mail->Port = 587;                                    
-
+    echo 2;
     //Recipients
     $mail->setFrom('quanhy2@example.com', 'duc quan');
     $mail->addAddress('nguyenducquanhy2@example.com', 'quan giai tich');     
     $mail->addReplyTo('quanhy2@example.com', 'quan');
-
+    echo 3;
     //Content
     $mail->isHTML(true);                                 
     $mail->Subject = 'Here is the subject';
     $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
+    echo 4;
     $mail->send();
-
+    echo 5;
     echo 'Message has been sent';
 } catch (Exception $e) {
     echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
