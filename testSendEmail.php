fc09d48b162292d3eb44283ad35fcc39118ccca6
nguyen duc quan <quanhy2@gmail.com>
2023-06-29 23:06:19 +0700

    update

--- testSendEmail.php
+++ testSendEmail.php
@@ -1,10 +1,5 @@
 <?php
-    header("Access-Control-Allow-Origin: *");
-    header("Access-Control-Allow-Headers: *");
-    header("Access-Control-Allow-Methods: *");
 
-include'library/cors.php';
-include'library/connect.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
