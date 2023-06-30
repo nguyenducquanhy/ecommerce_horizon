5b35efbdbebba39e2eeec74da81538cfe93321ce
nguyen duc quan <quanhy2@gmail.com>
2023-06-30 15:41:17 +0700

    update error code

--- testSendEmail.php
+++ testSendEmail.php
@@ -18,54 +18,63 @@ if($_SERVER['REQUEST_METHOD']==='POST'){
 
 function sendOTP(){
 
-// Đọc dữ liệu từ yêu cầu gửi email
-$otp =rand(10000,99999);
+    // Đọc dữ liệu từ yêu cầu gửi email
+    $otp =rand(10000,99999);
 
-//$address = 'nguyenducquanhy2@gmail.com';
-$address=$_POST['address'];
+    //$address = 'nguyenducquanhy2@gmail.com';
+    $address=$_POST['address'];
 
-$subject = $otp.' là mã xác nhận email của bạn';
-$body ='Chào bạn,<br>
-        Đây là mã code xác nhận email của bạn: '. $otp.'<br>
-        Thân,<br>
-        Team Horizon Tech';
+    if(!isset($address)) {
+        throw new Exception("Request mistake address.", 403);
+    }
+
+    $subject = $otp.' là mã xác nhận email của bạn';
+    $body ='Chào bạn,<br>
+            Đây là mã code xác nhận email của bạn: '. $otp.'<br>
+            Thân,<br>
+            Team Horizon Tech';
 
 
-// Khởi tạo một đối tượng PHPMailer
-$mail = new PHPMailer(true);
+    // Khởi tạo một đối tượng PHPMailer
+    $mail = new PHPMailer(true);
 
-try {
-    // Cấu hình máy chủ SMTP của Gmail
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'horizontechhh@gmail.com';  // Your Gmail address
-    $mail->Password = 'hvypolbxpfdbvcnw';   // Your App Password
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
+    try {
+        // Cấu hình máy chủ SMTP của Gmail
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'horizontechhh@gmail.com';  // Your Gmail address
+        $mail->Password = 'hvypolbxpfdbvcnw';   // Your App Password
+        $mail->SMTPSecure = 'tls';
+        $mail->Port = 587;
 
-    // Thiết lập thông tin người gửi và người nhận
-    $mail->setFrom('horizontechhh@gmail.com', 'Horizon Tech'); // Địa chỉ email và tên của bạn
-    $mail->addAddress($address); // Địa chỉ email người nhận
+        // Thiết lập thông tin người gửi và người nhận
+        $mail->setFrom('horizontechhh@gmail.com', 'Horizon Tech'); // Địa chỉ email và tên của bạn
+        $mail->addAddress($address); // Địa chỉ email người nhận
 
-    // Thiết lập nội dung email
-    $mail->CharSet = "UTF-8";
-    $mail->Encoding = 'base64';
-    $mail->isHTML(true);
-    $mail->Subject = $subject;
-    $mail->Body = $body;
+        // Thiết lập nội dung email
+        $mail->CharSet = "UTF-8";
+        $mail->Encoding = 'base64';
+        $mail->isHTML(true);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
 
-    // Gửi email
-    $mail->send();
+        // Gửi email
+        $mail->send();
 
-    echo json_encode(
-        Array("status"=>200,
-        "validCode"=>$otp),JSON_UNESCAPED_UNICODE );
-    
-} catch (Exception $e) {
-    echo json_encode( Array("status"=>500,
-    "validCode"=>null));
-}
+        echo json_encode(
+            Array("status"=>200,
+            "validCode"=>$otp),JSON_UNESCAPED_UNICODE );
+        
+    } catch (Exception $e) {
+        if($e->getMessage()=='Request mistake address.'){
+            echo $e->getMessage();
+            return;
+        }
+
+        echo json_encode( Array("status"=>500,
+        "validCode"=>null));
+    }
 }
 
 ?>
